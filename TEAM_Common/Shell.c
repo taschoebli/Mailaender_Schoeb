362e77a574108dedc6dc8add114edb10003a4215
Luzi Schoeb <schoeb.luzi@gmail.com>
2018-11-15 15:24:32 +0100

    Assignment 23, Queues added

--- TEAM_Common/Shell.c
+++ TEAM_Common/Shell.c
@@ -386,6 +386,15 @@ static void ShellTask(void *pvParameters) {
 #elif PL_CONFIG_HAS_SHELL_QUEUE /* !PL_CONFIG_SQUEUE_SINGLE_CHAR */
     {
       /*! \todo Handle shell queue */
+
+    	// Somebody must take the things in the queues
+    	const unsigned char *msg;
+
+    	msg=SQUEUE_ReceiveMessage();
+    	if(msg!=NULL){
+    	CLS1_SendStr(msg,CLS1_GetStdio()->stdOut);
+    	vPortFree((void*)msg);
+    	}
    }
 #endif /* PL_CONFIG_HAS_SHELL_QUEUE */
     vTaskDelay(pdMS_TO_TICKS(10));
